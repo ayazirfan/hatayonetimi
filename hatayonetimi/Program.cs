f29732980683920d224dbfcc168d116ea9dbdfec
ayazirfan <150783915+ayazirfan@users.noreply.github.com>
2023-11-18 00:55:15 +0300

    Add files via upload

---
+++ hatayonetimi/Program.cs
@@ -0,0 +1,35 @@
+﻿ try
+        {
+            Console.WriteLine("bir sayı giriniz");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+
+    public Program()
+    {
+    }
+
+    Console.WriteLine("girmiş olduğunuz sayı :" + sayı1);
+private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
+    catch (Exception ex)
+        {
+
+            Console.WriteLine("hata:" + ex.Message.ToString());
+
+        }
+
+    catch
+        {
+            (ArgumentNullException ex)
+            }
+
+     try
+        {
+            Console.WriteLine("boş değer giriniz");
+            Console.WriteLine(ex);
+        }
+
+
+
